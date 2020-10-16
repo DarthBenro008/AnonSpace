46fa89976ee6173b7f16a7019098b83b6f0a1f46
Hemanth Krishna <hkpdev008@gmail.com>
2020-10-17 03:59:29 +0530

    feat: bottom navigation bar

--- app/src/main/java/com/benrostudios/anonymouspace/ui/Home.kt
+++ app/src/main/java/com/benrostudios/anonymouspace/ui/Home.kt
@@ -2,11 +2,19 @@ package com.benrostudios.anonymouspace.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.benrostudios.anonymouspace.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        navController = findNavController(R.id.nav_host_fragment_home_activity)
+        bottom_navigation_view_home_activity.setupWithNavController(navController)
     }
 }
