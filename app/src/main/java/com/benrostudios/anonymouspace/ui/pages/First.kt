7b85e2a622777cf9d5264a75dfb14c5746e6c2d6
Hemanth Krishna <hkpdev008@gmail.com>
2020-10-18 02:28:35 +0530

    feat: animations

--- app/src/main/java/com/benrostudios/anonymouspace/ui/pages/First.kt
+++ app/src/main/java/com/benrostudios/anonymouspace/ui/pages/First.kt
@@ -1,11 +1,18 @@
 package com.benrostudios.anonymouspace.ui.pages
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.benrostudios.anonymouspace.R
+import com.benrostudios.anonymouspace.ui.ChatActivity
+import kotlinx.android.synthetic.main.fragment_first.*
 
 
 class First : Fragment() {
@@ -23,8 +30,38 @@ class First : Fragment() {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        join_button.setOnGenericMotionListener { view, motionEvent ->
+            requireActivity().startActivity(Intent(requireActivity(),ChatActivity::class.java))
+            true
+        }
+        base_motion.setTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                Log.d("hello","motion started")
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                Log.d("hello","motion started")
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                if(p1 == R.id.create_anim){
+                    requireActivity().startActivity(Intent(requireActivity(),ChatActivity::class.java))
+                    //requireActivity().finish()
+                }
+
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+                Log.d("hello","motion started")
+            }
+
+        })
+
     }
+
+
 }
